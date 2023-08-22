a6d6d6a298a0b63da9dc06bf09ca5bb8d4486107
Isabella Sanchis <isasanchis3@gmail.com>
2023-08-22 20:01:24 -0300

    feat: ajuste rota para home e módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from 'src/components/shared/shared.module';
+import { HomeModule } from 'src/components/home/home.module';
+import { RegisterModule } from 'src/components/register/register.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/home/home.module.ts
+++ src/components/home/home.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { SharedModule } from '../shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule,
+    BrowserModule
+  ],
+  bootstrap: [HomeComponent]
 })
 export class HomeModule { }
