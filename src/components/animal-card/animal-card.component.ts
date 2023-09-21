2160e15a5dc22634307ea8193902bf1cee17af1c
Isabella Sanchis <isasanchis3@gmail.com>
2023-09-21 15:19:52 -0300

    fix: ajustes de módulos e criação componente de animals cards

---
+++ src/components/animal-card/animal-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IAnimals } from 'src/models/animals';
+
+@Component({
+  selector: 'app-animal-card',
+  templateUrl: './animal-card.component.html',
+  styleUrls: ['./animal-card.component.scss']
+})
+export class AnimalCardComponent {
+  @Input() animals: IAnimals[] = [];
+  @Output() clickButton = new EventEmitter();
+
+}
---
+++ src/components/components.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from '../routes/app-routing.module';
+import { AppComponent } from '../app/app.component';
+import { SharedModule } from 'src/components/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from 'src/components/register/register.component';
+import { HomeComponent } from 'src/components/home/home.component';
+import { ToastrModule } from 'ngx-toastr';
+import { AnimalCardComponent } from 'src/components/animal-card/animal-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    AnimalCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class ComponentsModule { }
