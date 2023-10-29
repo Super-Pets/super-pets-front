6fac6cb0506dae05ecf209741d2647b093857b1c
Isabella Sanchis <isasanchis3@gmail.com>
2023-10-29 17:10:49 -0300

    feat: adição do deletar animal

--- src/components/animal-info/animal-info.component.ts
+++ src/components/animal-info/animal-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IAnimals } from 'src/models/animals';
+import { AnimalsService } from 'src/services/animals.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-animal-info',
@@ -10,14 +12,35 @@ export class AnimalInfoComponent {
   @Input() selectedAnimal: IAnimals | null = null;
   @Input() animalImages: any;
 
+  constructor(
+    private animalsService: AnimalsService,
+    private toastr: ToastrService
+  ) {}
+
   closeModal() {
     this.selectedAnimal = null;
   }
 
   displayImage(animal: IAnimals | null, images: any) {
     if (animal && animal.species) {
-      const matchImage = images.find((image: any) => image.species === animal.species);
+      const matchImage = images.find(
+        (image: any) => image.species === animal.species
+      );
       return matchImage?.img;
     }
+  }
+
+  deleteAnimal(id: number) {
+    this.animalsService.deleteAnimal(id).subscribe({
+      next: () => {
+        this.selectedAnimal = null;
+        location.reload();
+      },
+      error: () =>
+        this.toastr.error(
+          'Erro ao deletar pet. Por favor, tente novamente mais tarde.',
+          'Erro'
+        ),
+    });
   }
 }
